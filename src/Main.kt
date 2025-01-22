927f9014172d5d929d3ddd4a9ca73ce7e9e60031
Mario_Tamayo <trmario@iesrafaelalberti.es>
2025-01-22 17:31:06 +0100

    4.4 Refactor

--- src/Main.kt
+++ src/Main.kt
@@ -10,8 +10,8 @@ Recuerda que Kotlin añade los getters y setters con el comportamiento por defec
 
 El atributo matricula no debe permitir un valor que no tenga 7 caracteres.
 Los atributos modelo y marca siempre se devolverán con la primera letra en mayúscula (getter).
-El número de caballos deberá tener un valor entre 70 y 700.
-El número de puertas no podrá ser inferior a 3, ni superior a 5.
+El número de caballosdeberá tener un valor entre 70 y 700.
+El número de puertasno podrá ser inferior a 3, ni superior a 5.
 Ten en cuenta todas estas condiciones a la hora de crear el constructor de la clase.
 En el programa principal, instancia varios coches, muéstralos por pantalla (toString) y realiza lo siguiente:
 
@@ -20,47 +20,58 @@ Intenta instanciar un coche con el número de caballos con un valor inferior a 7
 Intenta instanciar un coche con el número de puertas con un valor inferior a 3 o superior a 5 y comprueba que no es posible.
 Intenta instanciar un coche con la matrícula con un valor que no tenga 7 caracteres y comprueba que no es posible.
 Intenta instanciar y modificar el color a un valor nulo y comprueba que no es posible.
-Todas estas pruebas debes hacerla capturando las excepciones que deben lanzarse desde la clase Coche cuando el valor de una propiedad no es correcto (según los resuisitos del ejercicio)
+Todas estas pruebas debes hacerla capturando las excepciones que deben lanzarse desde la clase Coche cuando el valor de una propiedad no es correcto (según los requisitos del ejercicio)
  */
 
-//TODO: Cambiar la actividad a la corregida por Diego (Revisar correo)
+//TODO: Ajustar la captura de excepciones (Revisar Readme.md)
 
-class Coche(var color: String, marca: String, modelo: String, cv: Int, matricula: String, nPuertas: Int) {
-    private val marca: String = marca
-        get() =  field.lowercase().replaceFirstChar { it.uppercase() }
+class Coche(color: String?, marca: String?, modelo: String?,
+            private val cv: Int?, private val matricula: String?, private val nPuertas: Int?) {
 
-    private val modelo: String = modelo
-        get() = field.lowercase().replaceFirstChar { it.uppercase() }
-
-    var cv: Int = 70
+    var color: String? = color
+        //En este set controlamos que lance una excepción solo en el caso en el que al modificarse sea nulo
         set(value) {
-            require(cv in 70..700)
+            if (value == null) throw IllegalArgumentException("El color no puede ser nulo")
             field = value
         }
 
-    var matricula: String = "000000A"
-        set(value) {
-            require(matricula.length == 7)
-            field = value
-        }
+    private var marca: String? = marca
+        get() = field?.lowercase()?.replaceFirstChar { it.uppercase() }
 
-    var nPuertas: Int = 3
-        set(value) {
-            require(nPuertas in 3..5)
-            field = value
-        }
+    private var modelo: String? = modelo
+        get() = field?.lowercase()?.replaceFirstChar { it.uppercase() }
 
     init {
-        require(this.marca.isNotEmpty())
-        require(this.modelo.isNotEmpty())
+        require(!marca.isNullOrEmpty())
+        require(!modelo.isNullOrEmpty())
+        require(matricula?.length == 7)
+        require(cv in 70..700)
+        require(nPuertas in 3..5)
+        require(color != null)
+    }
+
+    override fun toString(): String {
+        return "|Coche| Color: $color, Marca: $marca, Modelo: $modelo, Número de caballos: $cv, Matrícula: $matricula, Número de puertas: $nPuertas"
     }
 }
 
 fun main() {
-    val c1 = Coche("Blanco", "Nissan", "Juke", 150, "123456A", 5)
-    val c3 = Coche("Negro", "Ford", "Focus", 50, "000560C", 2)
-    val c4 = Coche("Naranja", "Seat", "Toledo", 120, "000", 5)
-    val c5 = Coche("Azul", "Volkswagen", "Golf", 150, "654321F", 20)
+    //Instanciar Objetos
+    val c1 = Coche(null, "nissan", "Juke", 150, "123456A", 5)
+    /*
+    val c2 = Coche("Negro", "", "Focus", 50, "000560C", 4)
+    val c3 = Coche("Naranja", "Seat", "Toledo", 120, "000000W", 3)
+    val c4 = Coche("Azul", "Volkswagen", "Golf", 150, "654321F", 20)
+    val c5 = Coche("Verde", "Peugeot", "205d", 60, "440625N", 4)
+    */
+    //Mostrar por pantalla
+    /*
+    println(c1.toString())
+    println(c2.toString())
+    println(c3.toString())
+    println(c4.toString())
+     */
 
-    println(c1)
+
+
 }
